f89e824e47fe49c1c4f343c028a048e4d01d290c
maggo <me@mar.co.de>
2024-04-04 17:11:58 +0200

    feat: implement feedback and tests

--- contracts/LooteryETHAdapter.sol
+++ contracts/LooteryETHAdapter.sol
@@ -4,14 +4,16 @@ pragma solidity 0.8.23;
 import {Lootery} from "./Lootery.sol";
 import {IWETH9} from "./interfaces/IWETH9.sol";
 
-contract LooteryNativeHelper {
-    IWETH9 public wrappedToken;
+contract LooteryETHAdapter {
+    IWETH9 public immutable wrappedToken;
+
+    event JackpotSeeded(address indexed whomst, uint256 amount);
 
     constructor(address payable _wrappedTokenAddress) {
         wrappedToken = IWETH9(_wrappedTokenAddress);
     }
 
-    function purchaseNative(
+    function purchase(
         address payable looteryAddress,
         Lootery.Ticket[] calldata tickets
     ) public payable {
@@ -19,24 +21,22 @@ contract LooteryNativeHelper {
         uint256 ticketsCount = tickets.length;
         uint256 totalPrice = lootery.ticketPrice() * ticketsCount;
 
-        require(msg.value >= totalPrice, "Insufficient funds");
+        require(msg.value == totalPrice, "Need to provide exact funds");
 
         wrappedToken.deposit{value: totalPrice}();
         wrappedToken.approve(looteryAddress, totalPrice);
 
         lootery.purchase(tickets);
-
-        if (msg.value > totalPrice) {
-            payable(msg.sender).transfer(msg.value - totalPrice);
-        }
     }
 
-    function seedJackpotNative(address payable looteryAddress) public payable {
+    function seedJackpot(address payable looteryAddress) public payable {
         Lootery lootery = Lootery(looteryAddress);
 
         wrappedToken.deposit{value: msg.value}();
         wrappedToken.approve(looteryAddress, msg.value);
 
         lootery.seedJackpot(msg.value);
+
+        emit JackpotSeeded(msg.sender, msg.value);
     }
 }
